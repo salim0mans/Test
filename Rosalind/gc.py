25ccd1a061efa030374f01995305ff3ef230375d
salim0mans <125513026+salim0mans@users.noreply.github.com>
2023-02-16 21:01:59 +0300

    Update gc.py

--- Rosalind/gc.py
+++ Rosalind/gc.py
@@ -1,8 +1,16 @@
+#In this code, I start with a function GCprocess that outputs CG percentage rounded as requested in Rosalind.
+
 def GCprocess(dna):
     dnau = dna.upper()
     x = (dnau.count("C")+dnau.count("G"))/(len(dnau))
     return round(100*x, 6)
 
+#I put the example dataset in gcdata.txt file (uploaded in the same folder with gc.py)
+##I ASSUME that if both files (gc.py and gcdata.txt) are downloaded, they are put in the same directory.
+#I introduced the wrangle funtion to manipulate the data, no matter how dna string is divided among newlines, as long as the same structure of FASTA is maintained.
+#wrangle will ouput a dictionary, whose keys are the IDs and values are the GC percentages.
+
+path = "data.txt"
 def wrangle(path):
     keydict = {}
     valdict = {}
@@ -25,6 +33,8 @@ def wrangle(path):
         v = list(valdict.keys())[i]
         pairdict[k] = v
     return pairdict
+
+#Finally I processed the last output to show maximum value with corresponding ID, the same way Rosalind Requested.
 
 def IDprocess(path):
     pairs = wrangle(path)
