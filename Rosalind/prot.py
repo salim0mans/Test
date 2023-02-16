9aca34e8d325c88c4cdf1ccb8cd944d7d18db160
salim0mans <125513026+salim0mans@users.noreply.github.com>
2023-02-16 22:33:52 +0300

    Update prot.py

--- Rosalind/prot.py
+++ Rosalind/prot.py
@@ -6,16 +6,18 @@ path = "protdata.txt"
 #First, I deploy the genetic code into a dictionary with values as lists
 #then along the string RNA, I move codon by codon.
 #For each codon, I run a loop to inspect which is the correct key (a.a.) in the code that contains such codon.
+## I put a warning for premature stop codon (nonsense mutation) that will stop all the process until repaired, However, if the user wants just to run the codes ignoring stop codon, They can just put (#) before line (32-34).
 
 code = {"A": ["GCU","GCC", "GCA","GCG"], "V": ["GUU","GUA","GUC","GUG"], "I": ["AUU", "AUC", "AUA"],"M":["AUG"], "L": ["CUU","CUC", "CUG", "CUA", "UUA","UUG"],"F": ["UUU", "UUC"], "S": ["UCU","UCC","UCA","UCG","AGU","AGC"], "P": ["CCU", "CCC","CCA","CCG"],"T": ["ACU","ACA","ACC","ACG"], "E": ["GAA","GAG"], "D":["GAU", "GAC"], "N":["AAU","AAC"],"K":["AAA","AAG"], "Q":["CAA", "CAG"], "H":["CAU","CAC"], "Y": ["UAU","UAC"], "C": ["UGU","UGC"], "W":["UGG"],"R":["CGA","CGC","CGU","CGG","AGA","AGG"], "G":["GGU","GGC","GGA","GGG"], "STOP":["UGA","UAA","UAG"]}
 aa = "ACDEFGHIKLMNPQRSTVWY"
 
 def translate(path):
     k = 3
-    peptide = [""]
+    peptide = []
+    final = []
     with open(path) as file:
         for line in file:
-            n = len(line)
+            n = len(line.rstrip())
             if n%3 != 0:
                 return "Error in length! final codon is not complete, or frame shift happened."
             for i in range(0,n-k+1,k):
@@ -28,7 +30,10 @@ def translate(path):
                     if codon in code[symbol]:
                         peptide.append(symbol)
                     elif codon in code["STOP"] and i+3 < n:
-                            return "".join(peptide)+"\n"+"Warning of premature Stop at position: " + str(i)
-    return "".join(peptide)
-### I also paid attention to stop codons, hence I added an output of stop codon position if happened before the end. 
+                            final.append("".join(peptide))
+                            return "\n".join(final)+"\n"+"Warning of premature Stop at position: " + str(i)
+            final.append("".join(peptide))
+            peptide = []
+    return "\n".join(final)
+
 print(translate(path))
